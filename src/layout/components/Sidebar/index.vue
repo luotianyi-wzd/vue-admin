eb414af8bd0dc80ac9fb3fcb44c25a26d18b8b1a
luotianyi-wzd <ltywzd@gmail.com>
2020-01-09 00:22:43 +0800

    20200109侧边栏导航样式

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,55 +1,64 @@
 <template>
-	<div :class="{'has-logo':showLogo}">
-		<logo v-if="showLogo" :collapse="isCollapse"/>
-		<el-scrollbar wrap-class="scrollbar-wrapper">
-			<el-menu
-					:default-active="activeMenu"
-					:collapse="isCollapse"
-					:background-color="variables.menuBg"
-					:text-color="variables.menuText"
-					:unique-opened="false"
-					:active-text-color="variables.menuActiveText"
-					:collapse-transition="false"
-					mode="vertical"
-			>
-				<sidebar-item v-for="route in permission_routes" :key="route.path" :item="route"
-				              :base-path="route.path"/>
-			</el-menu>
-		</el-scrollbar>
-	</div>
+    <div :style="{width:  isCollapse ? '64px' : '212px' }">
+        <el-menu default-active="1-4-1"
+                 :background-color="variables.menuBg"
+                 :text-color="variables.menuText"
+                 :unique-opened="false"
+                 :active-text-color="variables.menuActiveText"
+                 :collapse-transition="false"
+                 mode="vertical"
+                 :collapse="isCollapse">
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">导航一</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                    <span slot="title">选项4</span>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
 </template>
 
 <script>
-	import {mapGetters} from 'vuex'
-	import Logo from './Logo'
-	import SidebarItem from './SidebarItem'
-	// import variables from '@/styles/variables.scss'
-	
-	export default {
-		components: {SidebarItem, Logo},
-		computed: {
-			...mapGetters([
-				'permission_routes',
-				'sidebar'
-			]),
-			activeMenu() {
-				const route = this.$route
-				const {meta, path} = route
-				// if set path, the sidebar will highlight the path you set
-				if (meta.activeMenu) {
-					return meta.activeMenu
-				}
-				return path
-			},
-			showLogo() {
-				return this.$store.state.settings.sidebarLogo
-			},
-			variables() {
-				return variables
-			},
-			isCollapse() {
-				return !this.sidebar.opened
-			}
-		}
-	}
+    import variables from '@/styles/variables.less'
+
+    export default {
+        name: "SideBar",
+        data() {
+            return {
+                isCollapse: false
+            }
+        },
+        computed: {
+            variables() {
+                return variables
+            }
+        },
+        methods: {}
+    }
 </script>
+
+<style scoped lang="less">
+
+</style>
